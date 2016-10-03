aee571dcbeaac6ba892ed9d93e57d6d63509d9b2
Kristy Caster <kcaster@walmartlabs.com>
2016-10-03 10:00:36 -0700

    inital commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+ledPin = 8
+buttonPin = 10
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(ledPin, GPIO.OUT)
+#GPIO.setup(buttonPin, GPIO.IN)
+
+GPIO.output(ledPin, True)
+time.sleep(2)
+GPIO.output(ledPin, False)
+time.sleep(2)
+
+#
+# blink=0
+# while blink < 100:
+#     val = GPIO.input(buttonPin)
+#     if val:
+#         GPIO.output(ledPin, True)
+#         time.sleep(.5)
+#         GPIO.output(ledPin, False)
+#         time.sleep(.5)
+#         blink += 1
+#     else:
+#         GPIO.output(ledPin, True)
+
+GPIO.cleanup()
