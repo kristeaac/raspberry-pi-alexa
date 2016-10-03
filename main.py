444cf20d4ab88f50cade2834100ca64c050a6801
Kristy Caster <kcaster@walmartlabs.com>
2016-10-03 11:56:16 -0700

    add cleanup and exit method

--- main.py
+++ main.py
@@ -11,25 +11,32 @@ blue = 37
 led_pins = [red, green, blue]
 push_button = 10
 
+
 def setup():
     GPIO.setmode(GPIO.BOARD)
     for pin in led_pins:
         GPIO.setup(pin, GPIO.OUT)
     GPIO.setup(push_button, GPIO.IN)
 
+
 def cleanup():
     GPIO.cleanup()
+    exit()
+
 
 def blink(pin, duration=3):
     GPIO.output(pin, True)
     time.sleep(duration)
     GPIO.output(pin, False)
 
+
 def led_on(pin):
     GPIO.output(pin, True)
 
+
 def led_off(pin):
     GPIO.output(pin, False)
+
 
 def internet_on():
     print "Checking Internet Connection"
@@ -41,28 +48,27 @@ def internet_on():
         print "Connection Failed"
         return False
 
+
 def pressed():
     return not GPIO.input(push_button)
 
+
 if __name__ == "__main__":
     setup()
-    cleaned = False
     try:
         # check internet connection
         if internet_on():
             blink(blue)
         else:
             blink(red)
-            exit()
+            cleanup()
 
         # wait for push button to be pressed
         while True:
             if pressed():
                 led_on(green)
-	    else:
-		led_off(green)
+            else:
+                led_off(green)
     except:
         cleanup()
-        cleaned = True
-    if not cleaned:
-        cleanup()
+    cleanup()
