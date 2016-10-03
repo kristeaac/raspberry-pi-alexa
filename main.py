a90ddd62e20d1b225e0da9b5d049611b3a56d272
Kristy Caster <kcaster@walmartlabs.com>
2016-10-03 14:35:39 -0700

    show yellow when Alexa is speaking

--- main.py
+++ main.py
@@ -155,7 +155,9 @@ def alexa():
         with open(path + "response.mp3", 'wb') as f:
             f.write(audio)
         lock.release()
+        rgbLed.on(rgbLed.yellow)
         os.system('mpg123 -q {}1sec.mp3 {}response.mp3'.format(path, path))
+        rgbLed.off()
 
 
 def listen():
