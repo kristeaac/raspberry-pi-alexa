1460d59b2ce5ad4694659ed511093b9766c97452
Kristy Caster <kcaster@walmartlabs.com>
2016-11-14 10:18:27 -0800

    add weather use case

--- buttons.py
+++ buttons.py
@@ -3,6 +3,7 @@ from rgbled import RGBLed, Color
 import requests
 import os
 from avs import get_access_token, alexa
+import threading
 
 PUSH_BUTTON = 10
 
@@ -10,6 +11,7 @@ rgbLed = RGBLed(33, 35, 37)
 
 PATH = os.path.realpath(__file__).rstrip(os.path.basename(__file__))
 HELLO_MP3 = PATH + 'audio/hello.mp3'
+RECORDING_WAV = PATH + 'audio/recording.wav'
 
 
 def setup():
@@ -40,11 +42,39 @@ def greeting():
 def pressed():
     return not GPIO.input(PUSH_BUTTON)
 
+def ttw(text):
+    global RECORDING_WAV
+    os.system('echo \"{}\" | text2wave -o {}'.format(text, RECORDING_WAV))
+    return RECORDING_WAV
+
+def thinking(lock):
+    while lock.locked():
+        rgbLed.blink(Color.yellow)
+
+processing = False
+lock = threading.Lock()
+def start_thinking():
+    lock.acquire()
+    thinking_thread = threading.Thread(target=thinking, args=(lock, ))
+    thinking_thread.start()
+
+
+def end_thinking(response_file):
+    global processing
+    lock.release()  # stop blinking
+    print("Alexa is Ready to Speak")
+    rgbLed.on(Color.yellow)
+    speak(response_file)
+    rgbLed.off()
+    rgbLed.on(Color.white)
+    processing = False
+
 def run():
+    global processing
     while True:
-        if pressed():
-            rgbLed.blink(Color.yellow)
-        rgbLed.off()
+        if pressed() and not processing:
+            processing = True
+            alexa(ttw("What's the weather like in Seattle?"), start_thinking_callback=start_thinking, end_thinking_callback=end_thinking)
 
 def cleanup():
     print("Started GPIO Cleanup")
